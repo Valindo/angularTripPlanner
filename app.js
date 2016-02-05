c11f2b01c7775300f150c05e8e0509f1e6913d8c
valindo godinho <valindo.godinho@gmail.com>
2016-02-06 02:09:42 +0530

    Made changes to routes

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-var app = angular.module("myApp",['ui.router']);
+var app = angular.module("myApp",["ngRoute"]);
 
-app.config(function($routeProvider){
-	$routeProvider.when('/', {
-		templateUrl: 'views/search',
+// app.config(function($stateProvider, $urlRouterProvider){
+// 	// $urlRouterProvider.otherwise("/state1");
+// 	$stateProvider.state('test', {
+// 		url: "/",
+// 		templateUrl: "/views/search.html",
+// 		controller: 'searchCtrl'
+// 	});
+
+// })
+
+
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.when('/',{
+		templateUrl: 'views/search.html',
 		controller: 'searchCtrl'
-	});
-});
+	})
+}])
 
 
-app.controller('searchCtrl',['$scope',function($scope){
-	$scope.test = "man";
-	
+
+app.controller('searchCtrl',['$scope','$http',function($scope, $http){
+	$scope.data = {
+  "request": {
+    "passengers": {
+      "adultCount": 1
+    },
+    "slice": [
+      {
+        "origin": "GOI",
+        "destination": "BOM",
+        "date": "2016-02-06"
+      }
+    ]   
+    
+  }
+};
+
+
+$scope.test = function(){
+		$http.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCkLKyLgo71Fu_2pSaIQXfEQ5SsJHGcU5Q", $scope.data ).success(function( data, status, headers, config){
+			$scope.print = data;
+		console.log(data);
+	}).error(function(data,status,headers,config){
+		console.log("BRRR");
+	})
+}
 }])
 
 
