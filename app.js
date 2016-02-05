65b001b9c995d4e90e33db90df8f7d0339bdd65b
amoled27 <amodshinde1995@gmail.com>
2016-02-06 02:13:46 +0530

    non working zomato API

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var app = angular.module("myApp",['ui.router','$http']);
+var app = angular.module("myApp",['ui.router']);
 
-app.config(function($routeProvider){
-	$routeProvider.when('/', {
-		templateUrl: 'views/search',
-		controller: 'searchCtrl'
-	});
-});
+// app.config(function($routeProvider){
+// 	$routeProvider.when('/', {
+// 		templateUrl: 'views/search',
+// 		controller: 'searchCtrl'
+// 	});
+// });
 
 
 app.controller('searchCtrl',['$scope',function($scope){
@@ -18,21 +18,26 @@ app.controller('zomatoCtrl',[
 	'$http',
 	function($scope, $http){
 		// var categories = $resource('https://developers.zomato.com/api/v2.1/categories?user_key=b9a7f37a405a0b234d0e9b0714e80d1d')
-			var category = $http({
-			  method: 'GET',
-			  url: 'https://developers.zomato.com/api/v2.1/categories?user_key=b9a7f37a405a0b234d0e9b0714e80d1d'
+
+				$http({
+			  method: 'JSONP',
+			  
+			  headers: {	'Accept':'application/json',
+			  		'user_key':'b9a7f37a405a0b234d0e9b0714e80d1d'},
+			  url: 'https://developers.zomato.com/api/v2.1/categories'
 			}).then(function successCallback(response) {
 			    // this callback will be called asynchronously
 			    // when the response is available
 			    console.log('success');
-			    res.send();
+			    res.send(response);
 
-			  }, function errorCallback(response) {
-			    // called asynchronously if an error occurs
-			    // or server returns response with an error status.
-			  });
 
-	}
+			    	})
+
+			  
+}
+	
 
 
 	])
+
