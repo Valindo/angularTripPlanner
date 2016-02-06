01180d9725c74d9845dcc2f95b514453c14e6e81
nishitsardessai <nishit121@gmail.com>
2016-02-06 08:50:51 +0530

    vefewvaw

--- app.js
+++ app.js
@@ -87,6 +87,29 @@ $scope.test = function(){
 
 // 	])
 
+
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+
+	x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
 app.controller('foodCtrl',[
 	'$scope',
 	'$http',
