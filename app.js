7a5e3c811a5074704f4778a8924278cb2ce52581
valindo godinho <valindo.godinho@gmail.com>
2016-02-06 08:44:17 +0530

    Made UI changes and hooked the API to work

--- app.js
+++ app.js
@@ -32,11 +32,26 @@ app.controller('searchCtrl',['$scope','$http',function($scope, $http){
 
 
 $scope.test = function(){
-		$http.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCkLKyLgo71Fu_2pSaIQXfEQ5SsJHGcU5Q", $scope.data ).success(function( data, status, headers, config){
-			$scope.print = data;
-		console.log(data);
-	}).error(function(data,status,headers,config){
-		console.log("BRRR");
+	// 	$http.post("https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyCkLKyLgo71Fu_2pSaIQXfEQ5SsJHGcU5Q", $scope.data ).success(function( data, status, headers, config){
+	// 		$scope.print = data;
+	// 	console.log(data);
+	// }).error(function(data,status,headers,config){
+	// 	console.log("BRRR");
+	// })
+	// $scope.flightCode = $scope.flightDetails.match(/[A-Z]/g).replace(/,/g,"");
+	// console.log($scope.flightCode);
+	// $scope.flightNumber = parseInt($scope.flightDetails.match(/[0-9]/g).join().replace(/,/g,""));
+	// console.log($scope.flightNumber);
+	$scope.flightDetails = $scope.flightDetails.toUpperCase();
+	$scope.flightCode = String($scope.flightDetails).replace(/[0-9]/g,"");
+	$scope.flightNumber = parseInt($scope.flightDetails.replace(/[A-Z]/g,""));
+	$http({
+		method: "GET",
+		url: "https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/"+$scope.flightCode+"/"+$scope.flightNumber+"/dep/2016/02/06?appId=6a6c02d7&appKey=20e4b459aefb18c1b86e4bf9d26f223b&utc=false",	
+	}).then(function(response){
+		console.log(response.data);
+	}, function(response){
+
 	})
 }
 }]);
@@ -69,8 +84,5 @@ $scope.test = function(){
 
 			  
 // }
-	
-
-
-// ])
+// ]);
 
